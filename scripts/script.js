f78d1d5af4da1e416b1bd1a21a5bdbb5f587f87e
GeschichteProjekt <148541674+GeschichteProjekt@users.noreply.github.com>
2023-12-01 12:14:11 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -96,13 +96,12 @@ var ThirdQuiz = new Quiz(OptionRight, 2,
 //QUIZ AREA
 //QUIZ AREA
 
-
+FirstQuiz.ActivateQuiz();
 
 var CurrentQuizNumber = 0;
 QuizArray = [FirstQuiz, SecondQuiz];
 QuizArray = [FirstQuiz, SecondQuiz, ThirdQuiz];
-var CurrentQuiz = QuizArray[CurrentQuizNumber];
-CurrentQuiz.ActivateQuiz();
+var CurrentQuiz = QuizArray[CurrentQuizNumber]
 
 function ActiveNextQuiz() {
     CurrentQuizNumber = CurrentQuizNumber + 1;
