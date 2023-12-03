255379047773cc37419a776c555003fe713160d3
Kartoffel69420 <erik1114g@gmail.com>
2023-12-03 15:17:47 +0100

    last quiz change

--- scripts/script.js
+++ scripts/script.js
@@ -90,6 +90,38 @@ var ThirdQuiz = new Quiz(OptionRight, 2,
     "Die Regierung wollte (und fing auch an) Land in dem Karura Forest an Privatbesitzer zu verkaufen.",
     "Die Protestoren forderten, dass Staatspräsident Daniel Arap Moi abtritt und neue Wahlen gehalten werden.",
     "Sie wollten, dass Leute, die für ihre politische Meinungen inhaftiert wurden, freigelassen werden.")
+var FourthQuiz = new Quiz(OptionCenter, 3,
+    "Wangari Maathai gewann welchen Preis im Jahre 2004 welchen Preis?",
+    "Indira Ghandi Prize",
+    "Nobel Peace Prize",
+    "Juliet Hollister Award",
+    "Auszeichnung für außerordentlichen Beitrag zur internationalen Zusammenarbeit, Frieden und Entwicklung",
+    "Hoch anerkannte internationale Auszeichnung für außergewöhnliche Beiträge zur Förderung des Friedens",
+    "Anerkennung für Förderung von interreligiösem Verständnis und Frieden")
+var FifthQuiz = new Quiz(OptionCenter, 4,
+    "Was war der erste Preis, den Wangari Maathai gewonnen hatte?",
+    "Right Livelihood Award",
+    "Global 500 Roll of Honour",
+    "The Hunger Project’s Africa Prize for Leadership",
+    "Anerkennung für außergewöhnliche Leistungen im Bereich Umweltschutz und sozialer Gerechtigkeit",
+    "Ehrung für herausragende Beiträge zum Umweltschutz und zur Erhaltung der natürlichen Ressourcen",
+    "Auszeichnung für herausragende Führungsqualitäten im Kontext von Initiativen zur Überwindung von Hunger und Armut in Afrika")
+var SixthQuiz = new Quiz(OptionLeft, 5,
+    "Was hat laut Wangari Maathai Umwelt mit Demokratie und Frieden zu tun?",
+    "Ohne die Umwelt herrscht ein Resourcenmangel, der die Kultur einschränkt sowie zu Instabilität führt",
+    "Bäume (und das Green Belt Movement) sind ein Instrument, um die Frauen zu vereinigen um für die Demokratie und Frieden zu kämpfen",
+    "Nichts",
+    "Der Feuerholzmangel sowie das Austrocknen der Flüsse gefährdet die Natur-verbundene Kultur von Kenia. Dazu kommt, dass ein Resourcenmangel zu Internalen Konflikten zwischen Ethnien führen kann um zu überleben.",
+    "Wangari Maathai ging es nie um die Umwelt, sondern sie brauchte nur etwas um die Frauen von Kenia zu vereinigen, für etwas zu kämpfen, und somit hat die Natur nur indirekt mit Demokratie und Frieden zu tun",
+    "Wangari Maathai pflanzte Bäume, weil sie die Umwelt schätze, und nicht weil sie dachte, dass es die Demokratie oder den Frieden stärkt.")
+var SeventhQuiz = new Quiz(OptionRight, 6,
+    "Das Green Belt Movement...",
+    "stärkte die Umwelt",
+    "stärkte die Umwelt und Kultur",
+    "stärkte die Umwelt, Kultur und Demokratie",
+    "Das Green Belt Movement planzte hunderte Millionen von Bäumen, hielt sich aber aus den Themen Demokratie und Frieden raus, da es nunmal dafür nicht gegründet wurde.",
+    "Das Green Belt Movement half Frauen, ihre Traditionellen Kochrezepte wieder zu benutzen sowie den Mangel von Feuerholz zu besiegen.",
+    "Das Green Belt Movement fing als ein Movement an, bei dem Frauen Bäume pflanzten, wurde aber immer mehr zu einer Organisation, die Proteste organisierte um Frauenrechte, Demokratie, Kultur und Frieden zu verteidigen.")
 //QUIZ AREA
 //QUIZ AREA
 //QUIZ AREA
@@ -97,9 +129,9 @@ var ThirdQuiz = new Quiz(OptionRight, 2,
 FirstQuiz.ActivateQuiz();
 
 var CurrentQuizNumber = 0;
-var TotalQuizAmmount = 2;
-QuizArray = [FirstQuiz, SecondQuiz, ThirdQuiz];
-var CurrentQuiz = QuizArray[CurrentQuizNumber]
+var TotalQuizAmmount = 6;
+QuizArray = [FirstQuiz, SecondQuiz, ThirdQuiz, FourthQuiz, FifthQuiz, SixthQuiz, SeventhQuiz];
+var CurrentQuiz = QuizArray[CurrentQuizNumber];
 
 function ActiveNextQuiz() {
     CurrentQuizNumber = CurrentQuizNumber + 1;
